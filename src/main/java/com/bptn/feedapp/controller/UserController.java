3376ea1be4516867c7c4d4c1cbcf3c8d99a3e771
Christian Shingiro <cshingiro@sociallyradicalwebdesign.com>
2023-03-08 14:23:16 -0500

    Updating UserController to use JPA

--- src/main/java/com/bptn/feedapp/controller/UserController.java
+++ src/main/java/com/bptn/feedapp/controller/UserController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bptn.feedapp.jdbc.UserBean;
+import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.service.UserService;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,22 +31,22 @@ public class UserController {
     }
 
     @GetMapping("/")
-    public List<UserBean> listUsers() {
+    public List<User> listUsers() {
         logger.debug("The listUsers() method was invoked!");
         return this.userService.listUsers();
     }
 
     @GetMapping("/{username}")
-    public UserBean findByUsername(@PathVariable String username) {
+    public Optional<User> findByUsername(@PathVariable String username) {
         logger.debug("The findByUsername() method was invoked!, username={}", username);
         return this.userService.findByUsername(username);
     }
 
-
     @GetMapping("/{first}/{last}/{username}/{password}/{phone}/{emailId}")
-    public String createUser( @PathVariable String first, @PathVariable String last, @PathVariable String username, @PathVariable String password, @PathVariable String phone, @PathVariable String emailId) {
-        UserBean user = new UserBean();
-		
+    public String createUser(@PathVariable String first, @PathVariable String last, @PathVariable String username,
+            @PathVariable String password, @PathVariable String phone, @PathVariable String emailId) {
+        User user = new User();
+
         user.setFirstName(first);
         user.setLastName(last);
         user.setUsername(username);
@@ -54,13 +55,11 @@ public class UserController {
         user.setEmailId(emailId);
         user.setEmailVerified(false);
         user.setCreatedOn(Timestamp.from(Instant.now()));
-		
+
         logger.debug("The createUser() method was invoked!, user={}", user.toString());
-		
+
         this.userService.createUser(user);
-		
+
         return "User Created Successfully";
     }
 }
-    
-
