18cc1f45191be6c24440ddf890c04c7a7e828e59
Christian Shingiro <cshingiro@sociallyradicalwebdesign.com>
2023-03-21 10:43:31 -0400

    Change to User Service to include password reset

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -29,8 +29,14 @@ import com.bptn.feedapp.security.JwtService;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import org.springframework.http.HttpHeaders;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class UserService {
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     UserRepository userRepository;
 
@@ -130,5 +136,16 @@ public class UserService {
         headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username, this.provider.getJwtExpiration()));
 
         return headers;
+    }
+
+    public void sendResetPasswordEmail(String emailId) {
+
+        Optional<User> opt = this.userRepository.findByEmailId(emailId);
+
+        if (opt.isPresent()) {
+            this.emailService.sendResetPasswordEmail(opt.get());
+        } else {
+            logger.debug("Email doesn't exist, {}", emailId);
+        }
     }
 }
